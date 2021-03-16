0098e78a87f347574655ae0a9791dd60b2a86212
Zen121212 <34530295+Zen121212@users.noreply.github.com>
2021-03-16 03:05:20 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
+    </div>
+  );
+}
