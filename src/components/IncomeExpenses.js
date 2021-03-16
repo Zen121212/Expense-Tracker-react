8a00937c97870405d359a4d0a711cc1c165acd1a
Zen121212 <34530295+Zen121212@users.noreply.github.com>
2021-03-16 03:29:31 +0200

    Add Transaction

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,17 +1,13 @@
 export const IncomeExpenses = () => {
   return (
-    <div class="inc-exp-container">
+    <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p id="money-plus" class="money plus">
-          +$0.00
-        </p>
+        <p className="money plus">+$0.00</p>
       </div>
       <div>
         <h4>Expenses</h4>
-        <p id="money-minus" class="money minus">
-          -$0.00
-        </p>
+        <p className="money minus">-$0.00</p>
       </div>
     </div>
   );
