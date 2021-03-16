693e402d2581bca5b3e864f5dbf5ae8c0e0be345
Zen121212 <34530295+Zen121212@users.noreply.github.com>
2021-03-16 03:33:50 +0200

    IncomeExpenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -2,11 +2,11 @@ export const IncomeExpenses = () => {
   return (
     <div className="inc-exp-container">
       <div>
-        <h4>Income</h4>
+        <h4>INCOME</h4>
         <p className="money plus">+$0.00</p>
       </div>
       <div>
-        <h4>Expenses</h4>
+        <h4>EXPENSES</h4>
         <p className="money minus">-$0.00</p>
       </div>
     </div>
